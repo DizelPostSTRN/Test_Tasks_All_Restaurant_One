4034b0d14d7de277e2c7528b4ef93e79cf16c17c
DizelPostSTRN <ragno1@mail.ru>
2023-05-23 21:52:13 +0300

    v0.3

--- restaurant/restaurant_app/urls.py
+++ restaurant/restaurant_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'restaurant_app'
 
 urlpatterns = [
-    path('', ),
-    path('admin/', ),
+    path('', views.menu, name='menu'),
+    path('menu', views.menu, name='menu')
 ]
--- restaurant/restaurant_project/settings.py
+++ restaurant/restaurant_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'restaurant_app.apps.RestaurantConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
