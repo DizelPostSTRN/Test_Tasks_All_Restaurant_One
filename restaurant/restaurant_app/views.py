5496e4d30073f7d8d32aee4667895eff28468a0d
DizelPostSTRN <ragno1@mail.ru>
2023-05-25 20:35:06 +0300

    v0.5

--- restaurant/restaurant_app/views.py
+++ restaurant/restaurant_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Meal
 
 def menu(request):
-    # написать
-    return render(request, 'restaurant_app/menu.html')
+    meal_categories = list(filter(lambda el: 'NO_TYPE' not in el[0], Meal.MealType.choices))
+    return render(request, 'restaurant_app/menu.html', {'meal_categories': meal_categories})
