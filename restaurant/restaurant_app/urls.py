ae9d93663a28c9974014664bc1124f24bfc83a21
DizelPostSTRN <ragno1@mail.ru>
2023-05-25 20:45:24 +0300

    v0.7

--- restaurant/restaurant_app/urls.py
+++ restaurant/restaurant_app/urls.py
@@ -6,5 +6,5 @@ app_name = 'restaurant_app'
 urlpatterns = [
     path('', views.menu, name='menu'),
     path('menu', views.menu, name='menu'),
-    path('<meal_category>', views.menu, name='menu')
+    path('<meal_category>', views.meal_category, name='meal_category')
 ]
