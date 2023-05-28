c3727700c0a46df4ea6aab209c49b008ae377c88
DizelPostSTRN <ragno1@mail.ru>
2023-05-28 12:28:22 +0300

    v0.11

--- restaurant/restaurant_app/views.py
+++ restaurant/restaurant_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Meal
+from django.utils import timezone
 
 def menu(request):
     meal_categories = list(filter(lambda el: 'NO_TYPE' not in el[0], Meal.MealType.choices))
@@ -13,4 +14,12 @@ def meal_category(request, meal_category):
 
 def meal(request, meal_id):
     meal = Meal.objects.get(id=meal_id)
+    meal.mealclick_set.create(click_date=timezone.now())
     return render(request, 'restaurant_app/meal.html', {'meal': meal})
+
+# meal.mealclick_set.create(click_date=timezone.now) - Добавляем статисктику по кликам при
+# переходе на каждую страничку
+# (ссылаемся на класс class MealClick из models, меняем регистр на маленький)
+# mealclick_set что бы менять данные в моделях
+# По сути будет создаваться объект с датой когда пользователь нажал на какое нибудь блюдо
+
